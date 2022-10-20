b8ef4b4d8ea559cc2f68bcd7cca57a4b1d2008c3
PapitoPrograma <axelcaceres36@gmail.com>
2022-10-20 15:35:11 -0300

    navs y pet-list

---
+++ Views/pet-list.php
@@ -0,0 +1,54 @@
+<?php
+    include_once(VIEWS_PATH . "nav-user.php");
+?>
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <div class="content mb-3">
+                    <div>
+                        <a class="btn btn-success" href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                            </svg>
+                            Add Pet
+                        </a>
+                    </div>
+               </div>
+               <h2 class="mb-4">My Pet´s</h2>
+               <table class="table bg-light-alpha">
+                    <thead>
+                         <th>Name</th>
+                         <th>Breed</th>
+                         <th>Size</th>
+                         <th>Photo</th>
+                         <th>Vacunation Plan</th>
+                         <th>Vacunation Observations</th>
+                         <th>Details</th>
+                         <th>Actions</th>           
+                    </thead>
+                    <tbody>
+                         <form action="process/removeBeer.php" method="POST">
+                                <tr>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>
+                                        <a class="btn btn-info" href="">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                            </svg>
+                                        </a>
+                                        <button type="submit" name="btnRemove" class="btn btn-danger" value="1">X</button>
+                                    </td>
+                                </tr>
+                         </form>
+                    </tbody>
+               </table>
+          </div>
+     </section>
+</main>
