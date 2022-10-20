4769a70aa33988fcffaa6cd702c597fddde55161
PapitoPrograma <axelcaceres36@gmail.com>
2022-10-20 18:31:57 -0300

    Arreglos en vistas

--- Views/header.php
+++ Views/header.php
@@ -8,7 +8,5 @@
      <!-- Bootstrap CSS -->
      <link rel="stylesheet" href="<?php echo CSS_PATH ?>bootstrap.min.css">
      <link rel="stylesheet" href="<?php echo CSS_PATH?>estilos.css">
-
-     <title>Pet Hero</title>
 </head>
 <body>
