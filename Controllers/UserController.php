6feb7317248aaf8d96ed2ffe52db67c6875cd1c4
Mauro Triaca <maurotriaca7@gmail.com>
2022-10-21 00:41:03 -0300

    primer profile

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -13,8 +13,6 @@
         public function ShowProfileView()
         {
             require_once(VIEWS_PATH . "validate-session.php");
-          
-            $userDAO->GetByUserName($_SESSION["loggedUser"]);
             require_once(VIEWS_PATH . "profile.php");
         }
     }
